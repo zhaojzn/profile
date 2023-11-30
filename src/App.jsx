06d4864e55179aa8a34a1865aa8503afb6ea9e72
Jason <jasonnope123@gmail.com>
2023-11-29 23:31:43 -0800

    google ga

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import Home from "./pages/Home";
-
+import ReactGa from 'react-ga';
 function App() {
+
+  useEffect(() => {
+    ReactGa.initialize('G-7TSEF9YM8E')
+    ReactGa.pageview(window.location.pathname + window.location.search)
+  }, [])
 
   return (
     <Home/>
