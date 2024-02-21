bc7b4000669feb6ce0a11b46156d0269feffca33
Jason Zhao <jasonnope123@gmail.com>
2024-02-20 19:02:56 -0800

    measurement id change

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import Home from "./pages/Home";
 import ReactGA from 'react-ga4';
 function App() {
 
-
-  ReactGA.initialize('G-7TSEF9YM8E')
+  ReactGA.initialize('G-F8HG5RX9J6')
   ReactGA.send({
     hitType: 'pageview',
     page: window.location.pathname
