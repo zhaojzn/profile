bf99ae1254ef5d8581afc6d6a1655a07f86ea84f
Jason Zhao <jasonnope123@gmail.com>
2024-02-20 18:26:21 -0800

    page view update

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Home from "./pages/Home";
 import ReactGA from 'react-ga4';
 function App() {
 
-  useEffect(() => {
-    ReactGA.initialize('G-7TSEF9YM8E')
-    ReactGA.send({ hitType: "pageview", page: "/", title: "Welcome page" });
-  }, [])
 
+  ReactGA.initialize('G-7TSEF9YM8E')
+  ReactGA.send({
+    hitType: 'pageview',
+    page: window.location.pathname
+  })
   return (
     <Home/>
   );
