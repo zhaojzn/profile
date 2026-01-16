84c1c56e9c87c175c2015d946df158f904437ec1
Jason <jasonnope123@gmail.com>
2026-01-15 18:11:03 -0800

    profile update

--- src/components/Projects/ProjectList.jsx
+++ src/components/Projects/ProjectList.jsx
@@ -1,48 +1,49 @@
 import React from 'react'
-import z from '../../assets/z.png'
+import z from '../../assets/flickapp.png'
 import e from '../../assets/e.png'
 import d from '../../assets/d.png'
 
 const items = [
     {
-        title: "PGSS Chat",
+        title: "Flicka",
         description: (
             <>
-                My first ever project was a highschool project.
-                Initiated and completed my first independent web development project using Google Firebase and React JS. 
+                Developed a real-time social application for sharing moments.
+                Used firebase for instant data syncing and authentication,.
             </>
-          ),
+        ),
         image: z,
         link: "https://github.com/zhaojzn/pg-chat",
-        skills: ['React', 'Firebase', "Tailwind"]
+        skills: ['React Native', 'Firebase', "Tailwind", "AWS CDK"]
     },
     {
+
         title: "Wun2Free Schedule",
         description: (
             <>
                 Helped in the development of a employee scheduling website.
-                Implemented custom endpoints for the website using Go and AWS CDK. 
+                Implemented custom endpoints for the website using Go and AWS CDK.
             </>
-          ),
+        ),
         image: e,
         link: "https://github.com/jlui17/W2FHR",
         skills: ['React', 'Go', "AWS CDK"]
-    },  
+    },
     {
         title: "Spotify Suggestor",
         description: (
             <>
                 Created a Spotify song suggestion app for the employees at working at Wun2Free.
                 Connected AWS Cognito with the app to allow users to login and use the app.
-                <br/><br/>
+                <br /><br />
                 <b className='text-green-500'><a href='https://open.spotify.com/playlist/1R1gc0amaMvO8mi5yyKmY5?si=9065b3ddc0d046cd'>Press here to view the playlist</a></b>
-                <br/><br/>
+                <br /><br />
             </>
-          ),
+        ),
         image: d,
         link: "https://wun2playlist.netlify.app/",
         skills: ['React', "AWS CDK", "Firebase", "TypeScript"]
-    },  
+    },
 
 ]
 
--- src/lib/icons.js
+++ src/lib/icons.js
@@ -1,18 +1,19 @@
 const icons_dict = {
-    "AWS CDK": "devicon-amazonwebservices-original",
-    TypeScript: "devicon-typescript-plain",
-    Python: "devicon-python-plain",
-    Java: "devicon-java-plain",
-    "C#": "devicon-csharp-plain",
-    Angular: "devicon-angularjs-plain",
-    MSSQL: "devicon-microsoftsqlserver-plain",
-    ".NET": "devicon-dot-net-plain",
-    Jenkins: "devicon-jenkins-plain",
-    Github: "devicon-github-original",
-    "Next.js": "devicon-nextjs-plain",
-    React: "devicon-react-original",
-    Firebase: "devicon-firebase-plain",
-    Tailwind: "devicon-tailwindcss-plain",
-  };
-  
-  export default icons_dict;
+  "AWS CDK": "devicon-amazonwebservices-original",
+  TypeScript: "devicon-typescript-plain",
+  Python: "devicon-python-plain",
+  Java: "devicon-java-plain",
+  "C#": "devicon-csharp-plain",
+  Angular: "devicon-angularjs-plain",
+  MSSQL: "devicon-microsoftsqlserver-plain",
+  ".NET": "devicon-dot-net-plain",
+  Jenkins: "devicon-jenkins-plain",
+  Github: "devicon-github-original",
+  "Next.js": "devicon-nextjs-plain",
+  React: "devicon-react-original",
+  "React Native": "devicon-react-original",
+  Firebase: "devicon-firebase-plain",
+  Tailwind: "devicon-tailwindcss-plain",
+};
+
+export default icons_dict;
